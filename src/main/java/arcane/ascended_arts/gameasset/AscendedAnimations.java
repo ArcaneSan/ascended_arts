5b32188b958966af20281360383f5f3631081f5f
Mr Arcane <arcanekiros@gmail.com>
2025-05-31 22:01:57 -0700

    changes some names temp

--- src/main/java/arcane/ascended_arts/gameasset/AscendedAnimations.java
+++ src/main/java/arcane/ascended_arts/gameasset/AscendedAnimations.java
@@ -5,10 +5,13 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import yesman.epicfight.api.animation.AnimationClip;
 import yesman.epicfight.api.animation.AnimationManager;
 import yesman.epicfight.api.animation.property.AnimationProperty;
+import yesman.epicfight.api.animation.types.AttackAnimation;
 import yesman.epicfight.api.animation.types.BasicAttackAnimation;
 import yesman.epicfight.api.animation.types.DirectStaticAnimation;
 import yesman.epicfight.api.animation.types.StaticAnimation;
+import yesman.epicfight.api.utils.math.ValueModifier;
 import yesman.epicfight.gameasset.Armatures;
+import yesman.epicfight.world.damagesource.StunType;
 
 public class AscendedAnimations {
     public static DirectStaticAnimation EMPTY_ANIMATION = new DirectStaticAnimation() {
@@ -22,8 +25,8 @@ public class AscendedAnimations {
         }
     };
     public static AnimationManager.AnimationAccessor<BasicAttackAnimation> FLOATING_AUTO_1;
-    public static AnimationManager.AnimationAccessor<StaticAnimation> BIPED_HOLD_FLOATING_SABER;
-    public static AnimationManager.AnimationAccessor<BasicAttackAnimation> FLOATING_AUTO_2;
+    public static AnimationManager.AnimationAccessor<StaticAnimation> BIPED_HOLD_SABER;
+    public static AnimationManager.AnimationAccessor<BasicAttackAnimation> SABER_AUTO_2;
 
     @SubscribeEvent
     public static void registerAnimations(AnimationManager.AnimationRegistryEvent event) {
@@ -32,17 +35,21 @@ public class AscendedAnimations {
 
     public static void build(AnimationManager.AnimationBuilder builder) {
         //Every animation defined with his own hitbox timers and Properties
-        BIPED_HOLD_FLOATING_SABER = builder.nextAccessor("biped/living/idle_floating", (accessor) -> new StaticAnimation(true, accessor,Armatures.BIPED));
+        BIPED_HOLD_SABER = builder.nextAccessor("biped/living/saber_idle", (accessor) -> new StaticAnimation(true, accessor,Armatures.BIPED));
 
         //change times once they work
-        FLOATING_AUTO_1 = builder.nextAccessor("biped/combat/floating_auto_1", (accessor) ->
-                new BasicAttackAnimation(0.12F, 0.3F, 0.5F, 0.72F, 0.52F, null, Armatures.BIPED.get().toolR, accessor, Armatures.BIPED)
+        FLOATING_AUTO_1 = builder.nextAccessor("biped/combat/saber_auto_1", (accessor) ->
+                new BasicAttackAnimation(0.12F, 0.5F, 0.3F, 0.5F, 0.3F, null, Armatures.BIPED.get().toolR, accessor, Armatures.BIPED)
                         .addProperty(AnimationProperty.AttackAnimationProperty.BASIS_ATTACK_SPEED, 1.8F)
                         .addProperty(AnimationProperty.ActionAnimationProperty.CANCELABLE_MOVE, true));
-        FLOATING_AUTO_2 = builder.nextAccessor("biped/combat/floating_auto_2", (accessor) ->
-                new BasicAttackAnimation(0.12f, 0.3f, 0.5f, 0.72f, 0.52F, null, Armatures.BIPED.get().toolR, accessor, Armatures.BIPED)
-                        .addProperty(AnimationProperty.AttackAnimationProperty.BASIS_ATTACK_SPEED, 1.8F)
-                        .addProperty(AnimationProperty.ActionAnimationProperty.CANCELABLE_MOVE, true));
+        SABER_AUTO_2 = builder.nextAccessor("biped/combat/saber_auto_2", (accessor) ->
+                new BasicAttackAnimation(0.12F, accessor, Armatures.BIPED,
+                        new AttackAnimation.Phase(0.0F, 0.0F, 0.1F, 0.3F, 0.4F, 0.6F, Armatures.BIPED.get().toolR, null)
+                                .addProperty(AnimationProperty.AttackPhaseProperty.DAMAGE_MODIFIER, ValueModifier.multiplier(0.5F))
+                                .addProperty(AnimationProperty.AttackPhaseProperty.STUN_TYPE, StunType.NONE),
+                        new AttackAnimation.Phase(0.5F, 0.4F, 0.3F, 0.75F, 0.3F, 1.1F, Armatures.BIPED.get().toolR, null)
+                                .addProperty(AnimationProperty.AttackPhaseProperty.DAMAGE_MODIFIER, ValueModifier.multiplier(0.75F)))
+                        .addProperty(AnimationProperty.AttackAnimationProperty.BASIS_ATTACK_SPEED, 1.2F));
     }
 
 }
